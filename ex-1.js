5b03769cb52e5d0433186df4b59735933596a8ef
pang <pangton28.p@gmail.com>
2023-12-08 15:26:56 +0700

    feat(exercise): add built-in array functions 1

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,15 @@ let words = ["hello", "world"];
 
 function getWordLengths(words) {
   // Start coding here
+  // console.log(words.length);
+  // for (let i = 0; i < words.length; i++) {
+  //   console.log(words[i]);
+  //   console.log(words[i].length);
+  //   console.log(words.length);
+  //   let aaa = words[i].length
+  //   console.log(aaa);
+  // }
+  return words.map(words => words.length)
 }
 
 const result = getWordLengths(words);
