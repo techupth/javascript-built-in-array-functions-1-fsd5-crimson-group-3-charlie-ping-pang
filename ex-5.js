da62c6372c11d60c4bb8d369b1acc69a851eb130
CharlieHii <charlie_chaper@hotmail.com>
2023-12-08 11:31:36 +0700

    feat(all): charlie work

--- ex-5.js
+++ ex-5.js
@@ -7,6 +7,13 @@ const students = [
 
 function getAverageStudentScore(students) {
   // Start coding here
+  return (
+    students.reduce((accumulator, student) => {
+      return accumulator + student.score;
+    }, 0) / students.length
+  );
 }
 
 getAverageStudentScore(students); // Output: 87.5
+
+console.log(getAverageStudentScore(students));
